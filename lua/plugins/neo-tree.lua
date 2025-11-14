b257e635556ae8aacf0d224b913e5453d467043c
RodrigoCSoares <soares.c.rodrigo@gmail.com>
2025-11-14 11:51:23 +0100

    add octo plugin and remove gemini cli

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,8 +3,8 @@ return {
   opts = {
     filesystem = {
       filtered_items = {
-        visible = true,         -- Always show hidden files
-        hide_dotfiles = false,  -- Show dotfiles like .env, .gitignore
+        visible = true, -- Always show hidden files
+        hide_dotfiles = false, -- Show dotfiles like .env, .gitignore
         hide_gitignored = false, -- Show files ignored by git
       },
     },

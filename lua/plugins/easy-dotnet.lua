0cd7083c024966d165740594842caad849c47da5
RodrigoCSoares <soares.c.rodrigo@gmail.com>
2025-12-09 10:56:12 +0100

    enable builtin lsp easydotnet

--- lua/plugins/easy-dotnet.lua
+++ lua/plugins/easy-dotnet.lua
@@ -2,9 +2,16 @@ return {
   "GustavEikaas/easy-dotnet.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    'nvim-telescope/telescope.nvim',
+    "nvim-telescope/telescope.nvim",
   },
   config = function()
-    require("easy-dotnet").setup()
-  end
+    require("easy-dotnet").setup({
+      lsp = {
+        enabled = true, -- Enable builtin roslyn lsp
+        roslynator_enabled = true, -- Automatically enable roslynator analyzer
+        analyzer_assemblies = {}, -- Any additional roslyn analyzers you might use like SonarAnalyzer.CSharp
+        config = {},
+      },
+    })
+  end,
 }
